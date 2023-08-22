d33a7bd6b945116f1fd52e1b276bf3fac1817deb
Pame85 <pamellachua85@gmail.com>
2023-08-22 15:34:19 +0800

    station

---
+++ Station.rb
@@ -0,0 +1,19 @@
+class Station
+	def initialize(venue, distance, travel_fare)
+		@venue = venue 
+		@distance = distance
+		@travel_fare = travel_fare
+	end
+
+	def name
+		@name 
+	end
+
+	def distance
+		@distance 
+	end
+
+	def travel_fare
+		@travel_fare
+	end
+end
