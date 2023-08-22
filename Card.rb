76f3c07d3ddbc3685569d9ba6743fce8d185afeb
Pame85 <pamellachua85@gmail.com>
2023-08-22 15:32:49 +0800

    card

---
+++ Card.rb
@@ -0,0 +1,31 @@
+class Card
+	def initialize(balance)
+		@balance = balance
+	end
+
+	def card_types
+		student_card = student.card(-2)
+
+	def balance
+		@balance
+	end
+
+	def top_up_card(amount)
+		@balance += amount
+	end
+
+	def deduct_fare(amount)
+		@deduct_fare
+	end
+
+	def Expiry_card(expired)
+		expired = True
+		! expired = False
+		if true
+		puts "The card is expired! Please renew your card."
+		else False
+		puts "The card is valid!"
+		end
+	end
+
+end
